a5651d9623e858c385467d4d60abe0f40d0a2742
ivanata <32159121+ivanata@users.noreply.github.com>
2017-11-16 13:29:32 +0100

    part1

---
+++ c1/index.js
@@ -0,0 +1,18 @@
+console.log('hello world!')
+var ime ="Ivana";// string niza od karakteri
+var godini = 35;// number
+var visina = 182.5; //number
+var temperatura = -22.8; //number
+var classEnded = true; //boolen
+
+console.log(ime);
+console.log(godini);
+console.log(visina);
+console.log(temperatura);
+console.log(classEnded);
+
+godinaNaRaganje = 1989; // camel case style
+godina_na_raganje = 1989; //snake case style
+ godinanaraganje= 1989 //nikogas ne go koristiiiiii
+
+console.log( ime + " "+ prezime); 
