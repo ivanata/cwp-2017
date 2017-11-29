e05fea5bd397d8780678b919cbc11c813a148878
ivanata <32159121+ivanata@users.noreply.github.com>
2017-11-29 22:41:55 +0100

    js_1

---
+++ domasno1/index.js
@@ -0,0 +1,23 @@
+function c2t(c){
+	console.log(c*1.8+32 + ' ' + "F");
+}
+c2t(70);
+c2t(30);
+
+function f2c(n){
+	console.log((n-32)/1.8 + ' ' + 'C');
+}
+f2c(60);
+f2c(45);
+
+function temp1(a){
+ 	console.log(a + ' C'+ ' ' + (a*1.8+32) + ' '+'F')
+ }
+ temp1(20);
+ temp1(52);
+
+ function temp2(b){
+ 	console.log(b +"F"+" "+((b-32)/1.8)+"C");
+ }
+ temp2(60);
+ temp2(63);
