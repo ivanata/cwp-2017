9d6376f78b3cc124c93ab798dca50ed0ae143492
ivanata <32159121+ivanata@users.noreply.github.com>
2017-12-06 23:32:45 +0100

    domasna

---
+++ domasna2/index.js
@@ -0,0 +1,40 @@
+var niza = [2, 5, 3, 2, 3, 3, 5, 1, 1, 4, 4, 2, 4, 2, 2, 4];
+var a=0;
+function sekojTretParenBroj2(n){
+ 	for(var i=0; i< niza2.length; i++){
+ 		n=niza[i];
+ 		if (n%2==0) {
+ 				a=a+1;
+ 				if (a==3){ 
+ 					console.log(niza2[i]);
+ 					a=0;
+ 				};
+ 			};
+ 		};
+ 	}
+ 	
+ function zbirNaNeparniBroevi(niza){
+ 	sum=0;
+  	for(var i=0; i< niza.length; i++){
+ 		m=niza[i];
+ 		if (m%2==1) {
+ 		if (niza[i]%2==1) {
+  			sum=sum+niza[i];
+  		};
+  };
+  return "sum";
+ }	
+
+ var p=0;
+ function prosek(l){
+ function prosek(niza){
+ 	p=0;
+  	for(var i=0; i<niza.length; i++){
+ 		l=niza[i];
+ 		p=p+l;
+ 		p=p+niza[i];
+  	};
+ 	p=p/16;
+ 	p=p/niza.length;
+  	return 'p';
+  }	
---
+++ vezbi/index.js
@@ -0,0 +1,62 @@
+console.log('x'+' '+'y');
+console.log(2+2);
+console.log(4/2);
+console.log(6%3);
+console.log(7%3);
+
+var kolucuna_brasno=20;
+
+if (kolucuna_brasno>1){
+	console.log('ke pravime torta')
+}
+else{
+	console.log('nema da pravime torta')
+}
+
+var crno_brasno=5;
+var belo_brasno=12;
+
+
+if (crno_brasno<10){
+	console.log('ke pravime torta so crno brasno')
+}
+else if(belo_brasno<10){
+	console.log('ke pravime torta so belo brasno')
+
+}
+else{
+	console.log('nema da pravime torta')
+}
+
+var broj=15;
+
+if(broj%2==0){
+	console.log('brojot e paren');
+
+}
+else{
+	console.log('brjot e neparen');
+}
+var brasno=3;
+if (brasno<2){
+	console.log('imame brasno');
+
+}
+else{
+	console.log('nemame brasno')
+}
+var a=12;
+if(a<5 || a>10){
+	console.log('a e pomalo o 5 ili a e pogolemo od 10');
+}
+else{
+	console.log('a e vo opseg od 5 do 10');
+}
+
+
+niza5 = [2,5,3,2,3,5,1,1,4,4,2,4,2,2,4]
+for( i=0; i<niza5.lenght; i++)
+ var n=niza5[i]
+	if(n%2==0){
+		console.log('parni br')
+	}
